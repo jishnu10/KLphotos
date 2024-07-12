2f62806742dce55569bac932aa2843b40d1b10f6
Brav0 <68178115+jishnu10@users.noreply.github.com>
2024-07-12 19:24:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const imageContainer = document.getElementById('image-container');
-    const baseUrl = 'https://jishnu10.github.io/KLphotos1/images/'; // Replace with your GitHub Pages URL
+    const baseUrl = 'https://jishnu10.github.io/KLphotos/images/'; // Replace with your GitHub Pages URL
 
     // URL to the JSON file
-    const jsonUrl = 'https://jishnu10.github.io/KLphotos1/images.json'; // Replace with your GitHub Pages URL
+    const jsonUrl = 'https://jishnu10.github.io/KLphotos/images.json'; // Replace with your GitHub Pages URL
 
     fetch(jsonUrl)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             const images = data.images;
             images.forEach(image => {
@@ -16,5 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 imageContainer.appendChild(imgElement);
             });
         })
-        .catch(error => console.error('Error fetching images:', error));
+        .catch(error => {
+            console.error('Error fetching images:', error);
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = `Error fetching images: ${error.message}`;
+            imageContainer.appendChild(errorMessage);
+        });
 });
